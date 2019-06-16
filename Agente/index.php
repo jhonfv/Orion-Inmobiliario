18a0a4267b13c1b477e8387e35aec8bc9547bcd0
Jhon <jvelasco2@udi.edu.co>
2019-06-16 18:02:59 -0500

    Diseño de login

--- Agente/index.php
+++ Agente/index.php
@@ -2,18 +2,20 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/styles.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="theme-color" content="#000">
 	<title>Inicio</title>
 </head>
 <body>
-	<?php
-	echo("<p>Hola Mundo</p>");
-	?>
-	<form>
-		<input type="text" name="Usuario" placeholder="Usuario">
-		<input type="password" name="Contraseña" placeholder="Contraseña">
-		<input type="submit" name="Entrar" class="btn" value="Iniciar secion">
-	</form>
+	<div class="contenedor">
+		<img class="img" src="https://www.habitarinversiones.com/wp-content/uploads/2016/07/logo-habitar-e1468550422973.png">
+		<form action="" class="formulario" id="formulario" name="formulario">
+			<input class="formulario-input" type="text" name="Usuario" placeholder="Usuario">
+			<input class="formulario-input" type="password" name="Contraseña" placeholder="Contraseña">
+			<input type="submit" name="Entrar" class="btn" value="Iniciar secion">
+		</form>
+	</div>
 </body>
 </html>
